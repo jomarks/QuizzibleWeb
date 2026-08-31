d53567eb092c384b9346abf60d02c76c6f2a5a64
Joel Marks <joelmarks@gmail.com>
2026-08-30 22:15:32 -0500

    Deploy from BibleQuizApp - 2026-08-30 22:15:32

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1788139823'; // Will be replaced during build
+const CACHE_VERSION = '1788146132'; // Will be replaced during build
 const CACHE_NAME = `quizzible-${CACHE_VERSION}`;
 const BASE_PATH = '';
 
